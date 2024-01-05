3b652d4a78b29b531223f5a66f15182d1ac26f3f
chaitu0417 <111651658+chaitu0417@users.noreply.github.com>
2024-01-05 13:30:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
